70790b7454cc539c0546e02f7aedd1c714a009bb
Rommel <rommel.rivera022@gmail.com>
2024-01-07 21:19:35 +0800

    New update

---
+++ src/server/api/root.ts
@@ -0,0 +1,19 @@
+import { postRouter } from "~/server/api/routers/post";
+import { createTRPCRouter } from "~/server/api/trpc";
+import { topicRouter } from "./routers/topic";
+import { noteRouter } from "./routers/note";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  post: postRouter,
+  topic: topicRouter,
+  note: noteRouter
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from "tailwindcss";
+
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+} satisfies Config;
